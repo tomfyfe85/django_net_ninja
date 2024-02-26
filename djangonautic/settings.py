799359ad93caeadac0e1a26fd44f5fcf399ecf2e
Tom Fyfe <tomfyfe85@gmail.com>
2024-02-26 14:03:04 +0000

    created djangonautic database, need to sort out psycopg

--- djangonautic/settings.py
+++ djangonautic/settings.py
@@ -75,9 +75,12 @@ WSGI_APPLICATION = "djangonautic.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangonautic',  # Name of your PostgreSQL database
+        'USER': 'tomfyfe',    # Your PostgreSQL username
+        'HOST': 'localhost',   # Host where PostgreSQL is running (default: localhost)
+        'PORT': '5432',        # PostgreSQL port (default: 5432)
     }
 }
 

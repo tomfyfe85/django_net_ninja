d4e2c67076a11e7a67ba31720ec5e68f232507ed
Tom Fyfe <tomfyfe85@gmail.com>
2024-02-26 20:46:23 +0000

    psql is setup with django

--- articles/models.py
+++ articles/models.py
@@ -6,6 +6,6 @@ class Articles(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     body = models.TextField()
-    date = models.DateTimeField(auto_now_add = True)
+    date = models.DateTimeField(auto_now_add=True)
     # add in thumbnail later
     # add in author later

e260d491184ead7ebf30e9f1139c0cc8bc49cc38
Tom Fyfe <tomfyfe85@gmail.com>
2024-03-01 15:45:59 +0000

    still working on lesson 14 - URL paramters

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path("", views.article_list),
-]
+urlpatterns = [path("", views.article_list), path("<slug: article>", views.article_detail)]

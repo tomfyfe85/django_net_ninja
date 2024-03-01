4aa7461f47124b2853a4b57254c28ff40b67a757
Tom Fyfe <tomfyfe85@gmail.com>
2024-03-01 13:01:25 +0000

    lesson #11 complete

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Article
 
 
 # Create your views here.
 def article_list(request):
-    return render(request, "articles/article_list.html")
+    articles = Article.objects.all().order_by("date")
+    hello = "hi!"
+    return render(request, "articles/article_list.html", {"articles": articles})

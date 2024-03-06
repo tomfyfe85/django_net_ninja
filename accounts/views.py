f9efa80b0875aff64ac4aa3707b974577b6bb03c
Tom Fyfe <tomfyfe85@gmail.com>
2024-03-06 20:07:37 +0000

    lesson 23 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 
 # Create your views here.
@@ -7,8 +8,10 @@ def signup_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             # log user in
+            login(request, user)
+
             return redirect("articles:list")
     else:
         form = UserCreationForm()
@@ -19,7 +22,8 @@ def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # log in user
+            user = form.get_user()
+            login(request, user)
             return redirect("articles:list")
 
     else:

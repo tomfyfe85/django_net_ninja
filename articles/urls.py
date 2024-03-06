ae0fcb4ee0b765210bb9b301e2a1a0e3011c673a
Tom Fyfe <tomfyfe85@gmail.com>
2024-03-06 11:00:36 +0000

    lesson 16 complete

--- articles/urls.py
+++ articles/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.article_list),
-    path("<slug:slug>", views.article_detail),
+    path("", views.article_list, name="list"),
+    path("<slug:slug>", views.article_detail, name="detail"),
 ]

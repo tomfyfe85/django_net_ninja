42f4a11de5b2b281d33344cd7cf1fd500740b25b
Tom Fyfe <tomfyfe85@gmail.com>
2024-03-06 19:02:48 +0000

    lesson 21 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = "accounts"
 
-urlpatterns = [path("signup/", views.signup_view, name="signup")]
+urlpatterns = [
+    path("signup/", views.signup_view, name="signup"),
+    path("login/", views.login_view, name="login"),
+]
